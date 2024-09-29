ffb2cba1f708f184bbabe61d45ce7fd183f492c4
Naimul <64520358+Nai-mul@users.noreply.github.com>
2024-09-30 01:44:36 +0600

    Update launcher.py

--- bot/utils/launcher.py
+++ bot/utils/launcher.py
@@ -1,16 +1,18 @@
+import os
+import glob
 import asyncio
 import argparse
 from itertools import cycle
 
-from pyrogram import Client, compose
+from pyrogram import Client
+from better_proxy import Proxy
 
 from bot.config import settings
 from bot.utils import logger
 from bot.core.tapper import run_tapper
 from bot.core.registrator import register_sessions
-from bot.utils.scripts import get_session_names, get_proxies
 
-banner = """
+start_text = """
 
 
 ███████╗██╗███████╗     ██████╗ ██████╗ ██████╗ ███████╗██████╗ 
@@ -20,21 +22,20 @@ banner = """
 ███████║██║███████╗    ╚██████╗╚██████╔╝██████╔╝███████╗██║  ██║
 ╚══════╝╚═╝╚══════╝     ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝
 
-NotBoredPuppies BOT V 1.0
+Boinkers BOT V 1.0
 Prepared and Developed by: F.Davoodi
-
 """
-
-options = """
-Select an action:
-
-    1. Create session
-    2. Run clicker
-    3. Run via Telegram (Beta)
-"""
-
 
 global tg_clients
+
+
+def get_session_names() -> list[str]:
+    session_names = sorted(glob.glob("sessions/*.session"))
+    session_names = [
+        os.path.splitext(os.path.basename(file))[0] for file in session_names
+    ]
+
+    return session_names
 
 
 async def get_tg_clients() -> list[Client]:
@@ -43,7 +44,7 @@ async def get_tg_clients() -> list[Client]:
     session_names = get_session_names()
 
     if not session_names:
-        raise FileNotFoundError("Not found session files")
+        return None  # No sessions found
 
     if not settings.API_ID or not settings.API_HASH:
         raise ValueError("API_ID and API_HASH not found in the .env file.")
@@ -63,57 +64,32 @@ async def get_tg_clients() -> list[Client]:
 
 
 async def process() -> None:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--action", type=int, help="Action to perform")
+    print(start_text)
 
-    print(banner)
+    tg_clients = await get_tg_clients()
 
-    logger.info(f"Detected {len(get_session_names())} sessions | {len(get_proxies())} proxies")
-
-    action = parser.parse_args().action
-
-    if not action:
-        print(options)
-
-        while True:
-            action = input("> ")
-
-            if not action.isdigit():
-                logger.warning("Action must be number")
-            elif action not in ["1", "2", "3"]:
-                logger.warning("Action must be 1, 2 or 3")
-            else:
-                action = int(action)
-                break
-
-    if action == 1:
+    if tg_clients is None:
+        print("No sessions found. Please create a session first.")
         await register_sessions()
-    elif action == 2:
-        tg_clients = await get_tg_clients()
-
+    else:
         await run_tasks(tg_clients=tg_clients)
-    elif action == 3:
-        tg_clients = await get_tg_clients()
-
-        logger.info("Send /help command in Saved Messages\n")
-
-        await compose(tg_clients)
 
 
 async def run_tasks(tg_clients: list[Client]):
     proxies = get_proxies()
     proxies_cycle = cycle(proxies) if proxies else None
-    lock = asyncio.Lock()
-
     tasks = [
         asyncio.create_task(
             run_tapper(
                 tg_client=tg_client,
                 proxy=next(proxies_cycle) if proxies_cycle else None,
-                lock=lock,
             )
         )
         for tg_client in tg_clients
     ]
 
     await asyncio.gather(*tasks)
+
+
+if __name__ == "__main__":
+    asyncio.run(process())
